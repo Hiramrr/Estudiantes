f8d7e3cbddcb91785af91579c629c6a2067ed51a
Hiramrr <sasuke_77.23@hotmail.com>
2024-10-28 12:23:25 -0600

    Nuevo

---
+++ src/main/java/com/mycompany/estudiantes/Estudiantes.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+package com.mycompany.estudiantes;
+
+
+/**
+ *
+ * @author sasuk
+ */
+public class Estudiantes {
+
+    public static void main(String[] args) {
+        Principal start =  new Principal();
+        start.setVisible(true);
+    }
+}
